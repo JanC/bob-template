4f75dd7c349dfc2b54a5a217dc7b6dd9a064bc73
Jan Chaloupecky <jan.chaloupecky@gmail.com>
2018-10-17 15:52:46 +0200

    Added  BumpCommand and AlignVersionCommand templates

--- Sources/Run/main.swift
+++ Sources/Run/main.swift
@@ -11,4 +11,29 @@ let bob = Bob(config: config, droplet: drop)
 // Register a custom command defined in the `Commands` target.
 try bob.register(BonjourCommand())
 
+// Configure and register the built in BumpCommand
+let githubUser = "<username>"
+let githubRepo = "<repo>"
+let personalAccessToken = "<token>"
+let github = GitHub(config: .init(username: githubUser,
+                                  personalAccessToken: personalAccessToken,
+                                  repoUrl: "https://api.github.com/repos/\(githubUser)/\(githubRepo)"),
+                    droplet: drop)
+
+// A list of files to bump the version
+let plistPaths: [String] = [
+    "App/Info.plist",
+    "siriKit/Info.plist",
+    "siriKitUI/Info.plist"
+]
+let author = Author(name: "<commit user name>", email: "<commit email>")
+
+
+// Configure and register the built in BumpCommand
+try bob.register(BumpCommand(gitHub: github, plistPaths: plistPaths, author: author))
+
+
+// configure and register the AlignVersionCommand
+try bob.register(AlignVersionCommand(gitHub: github, plistPaths: plistPaths, author: author))
+
 try bob.start()
